df7136f0cbfce241225f3705b558bdeb71d57c8e
marat1000 <marat76593@gmail.com>
2022-02-05 21:43:15 +0100

    Добавляет верстку блока преимуществ

--- source/js/script.js
+++ source/js/script.js
@@ -1,28 +1,26 @@
 'use strict';
 
-// accordion
+// Accordion in the footer
 
-const accordionElements = document.querySelectorAll('.page-footer__accordion');
+const accordionItems = document.querySelectorAll('.page-footer__accordion');
 const accordionToggles = document.querySelectorAll('.page-footer__accordion h3');
 const mobileScreen = window.matchMedia('(max-width: 767px)');
 
-if (accordionElements) {
-  for (let i = 0; i !== accordionElements.length; i++) {
-    accordionElements[i].classList.remove('page-footer__accordion_nojs');
+if (accordionItems) {
+  for (let i = 0; i !== accordionItems.length; i++) {
+    accordionItems[i].classList.remove('page-footer__accordion_nojs');
   }
 }
 if (accordionToggles) {
   for (let i = 0; i !== accordionToggles.length; i++) {
     accordionToggles[i].addEventListener('click', function (evt) {
-      evt.preventDefault();
       if (mobileScreen.matches) {
         let array = Array.from(accordionToggles);
         let index = array.indexOf(evt.target);
-        accordionElements[index].classList.toggle('page-footer__accordion_active');
-
+        accordionItems[index].classList.toggle('page-footer__accordion_active');
         array.forEach(function (item, j) {
           if (j !== index) {
-            accordionElements[j].classList.remove('page-footer__accordion_active');
+            accordionItems[j].classList.remove('page-footer__accordion_active');
           }
         });
       }
