cc5dd5e18466598b30fd320b0c19905e7dbaf265
marat1000 <marat76593@gmail.com>
2022-02-09 11:18:22 +0100

    Добавляет верстку модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,9 @@
 
 // Accordion in the footer
 
-const accordionItems = document.querySelectorAll('.page-footer__accordion');
-const accordionToggles = document.querySelectorAll('.page-footer__accordion h3');
-const mobileScreen = window.matchMedia('(max-width: 767px)');
+let accordionItems = document.querySelectorAll('.page-footer__accordion');
+let accordionToggles = document.querySelectorAll('.page-footer__accordion h3');
+let mobileScreen = window.matchMedia('(max-width: 767px)');
 
 if (accordionItems) {
   for (let i = 0; i !== accordionItems.length; i++) {
@@ -27,3 +27,49 @@ if (accordionToggles) {
     });
   }
 }
+
+// Modal
+
+let buttonModal = document.querySelector('.page-header button');
+let modal = document.querySelector('.modal');
+let modalClose = document.querySelector('.modal__close');
+let overlay = document.querySelector('.overlay');
+
+buttonModal.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.add('modal__opened');
+  overlay.classList.add('overlay__show');
+  let closePopups = () => {
+    modal.classList.remove('modal__opened');
+    overlay.classList.remove('overlay__show');
+    overlay.removeEventListener('click', modalsClickClose);
+    document.removeEventListener('keydown', onFormEscKeydown);
+  };
+  modalClose.addEventListener('click', () => {
+    closePopups();
+  });
+  let onFormEscKeydown = (event) => {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+      event.preventDefault();
+      closePopups();
+    }
+  };
+  document.addEventListener('keydown', onFormEscKeydown);
+  let modalsClickClose = (event) => {
+    event.preventDefault();
+    closePopups();
+  };
+  overlay.addEventListener('click', modalsClickClose);
+});
+
+// localStorage
+
+let formInputs = document.querySelectorAll('.form input, .form textarea');
+
+formInputs.forEach((item) => {
+  item.value = localStorage.getItem(item.getAttribute('id'));
+  item.addEventListener('input', () => {
+    localStorage.setItem(item.getAttribute('id'), item.value);
+  });
+});
+
